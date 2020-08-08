5a0fbbf01fc272a802c4bbcef953b6c9729b244f
skarfie123 <rahulpai@rocketmail.com>
2020-08-08 16:39:45 +0100

    Updates

---
+++ precache-manifest.daa0a9d2fd4733201460d6d7dacb636f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f7c5aec47effb048bda6008293347d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30115f9e53af330c7b20",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2e49169e45187b3ec3d3",
+    "url": "/static/js/2.eed150c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eed150c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30115f9e53af330c7b20",
+    "url": "/static/js/main.40d907bd.chunk.js"
+  },
+  {
+    "revision": "7653aed456a86fc75601",
+    "url": "/static/js/runtime-main.33db18b4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
