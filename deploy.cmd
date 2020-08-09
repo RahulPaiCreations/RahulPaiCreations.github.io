c1445477bbeb3533f30a36546c22f37562b0426b
skarfie123 <rahulpai@rocketmail.com>
2020-08-09 17:41:30 +0100

    better deploy, fix background

--- deploy.cmd
+++ deploy.cmd
@@ -1,8 +1,8 @@
 @echo off
-echo "Build"
+echo "<--- Build --->"
 call npm run build
-echo "Commit Build"
-call git commit -a -m "Build"
-echo "Push"
+echo "<--- Commit Build --->"
+if "%~1"=="" (call git commit -a -m "Build") else (call git commit -a -m "%~1")
+echo "<--- Push --->"
 call git push
-echo "Done"
+echo "<--- Done --->"
